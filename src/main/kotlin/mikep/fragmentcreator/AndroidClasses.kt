a8af3ec539a7de61bad1c0a5e1e30a70c9c9c53a
MikeSchoolAccnt <purcell.14@wright.edu>
2019-05-04 21:40:30 -0400

    Add fully creating the fragment class and saving it to a file

--- src/main/kotlin/mikep/fragmentcreator/AndroidClasses.kt
+++ src/main/kotlin/mikep/fragmentcreator/AndroidClasses.kt
@@ -1,43 +1,43 @@
 package mikep.fragmentcreator
 
-import com.squareup.kotlinpoet.ClassName
-
+import com.squareup.kotlinpoet.*
 
 
 object AndroidClasses {
 
-    private val packageRegex = Regex("[A-Za-z]+\\.*")
+    private val packageRegex = Regex("[A-Za-z]+[.].*")
     //TODO Get all of the possible classes from xml that don't contain their package in their name
 
-    val view = ClassName("android.view.View", "View")
+    val view = ClassName("android.view", "View")
 
     //Layouts
-    val linearLayout = ClassName("android.widget.LinearLayout", "LinearLayout")
-    val frameLayout = ClassName("android.widget.FrameLayout", "FrameLayout")
-    val tableLayout = ClassName("android.widget.TableLayout", "TableLayout")
-    val tableRow = ClassName("android.widget.TableRow", "TableRow")
-    val viewGroup = ClassName("android.view.ViewGroup", "ViewGroup")
+    val linearLayout = ClassName("android.widget", "LinearLayout")
+    val frameLayout = ClassName("android.widget", "FrameLayout")
+    val tableLayout = ClassName("android.widget", "TableLayout")
+    val tableRow = ClassName("android.widget", "TableRow")
+    val viewGroup = ClassName("android.view", "ViewGroup")
 
 
     //Views
-    val button = ClassName("android.widget.Button", "Button")
-    val imageView = ClassName("android.widget.ImageView", "ImageView")
-    val scrollView = ClassName("android.widget.ScrollView", "ScrollView")
-    val switch = ClassName("android.widget.Switch", "Switch")
-    val imageButton = ClassName("android.widget.ImageButton", "ImageButton")
-    val checkBox = ClassName("android.widget.CheckBox", "CheckBox")
-    val radioGroup = ClassName("android.widget.RadioGroup", "RadioGroup")
-    val radioButton = ClassName("android.widget.RadioButton", "RadioButton")
-    val webView = ClassName("android.webkit.WebView", "WebView")
-    val videoView = ClassName("android.widget.VideoView", "VideoView")
-    val calendarView = ClassName("android.widget.CalendarView", "CalendarView")
-    val progressBar = ClassName("android.widget.ProgressBar", "ProgressBar")
-    val seekBar = ClassName("android.widget.SeekBar", "SeekBar")
-    val ratingBar = ClassName("android.widget.RatingBar", "RatingBar")
-    val searchView = ClassName("android.widget.SearchView", "SearchView")
-    val textureView = ClassName("android.view.TextureView", "TextureView")
-    val surfaceView = ClassName("android.view.SurfaceView", "SurfaceView")
-    val spinner = ClassName("android.widget.Spinner", "Spinner")
+    val button = ClassName("android.widget", "Button")
+    val imageView = ClassName("android.widget", "ImageView")
+    val scrollView = ClassName("android.widget", "ScrollView")
+    val switch = ClassName("android.widget", "Switch")
+    val imageButton = ClassName("android.widget", "ImageButton")
+    val checkBox = ClassName("android.widget", "CheckBox")
+    val radioGroup = ClassName("android.widget", "RadioGroup")
+    val radioButton = ClassName("android.widget", "RadioButton")
+    val webView = ClassName("android.webkit", "WebView")
+    val videoView = ClassName("android.widget", "VideoView")
+    val calendarView = ClassName("android.widget", "CalendarView")
+    val progressBar = ClassName("android.widget", "ProgressBar")
+    val seekBar = ClassName("android.widget", "SeekBar")
+    val ratingBar = ClassName("android.widget", "RatingBar")
+    val searchView = ClassName("android.widget", "SearchView")
+    val textureView = ClassName("android.view", "TextureView")
+    val surfaceView = ClassName("android.view", "SurfaceView")
+    val spinner = ClassName("android.widget", "Spinner")
+    val textView = ClassName("android.widget", "TextView")
 
 
     fun classNameFromNodeName(nodeName: String) : ClassName {
@@ -79,8 +79,24 @@ object AndroidClasses {
             "TextureView" -> textureView
             "SurfaceView" -> surfaceView
             "Spinner" -> spinner
+            "TextView" -> textView
             else -> throw RuntimeException("No class for $viewName!")
         }
+    }
+
+    fun createFragmentClass(className: String, layoutId: String) : TypeSpec.Builder {
+        val clazz = TypeSpec.classBuilder(className).superclass(ClassName("androidx.fragment.app", "Fragment"))
+        clazz.addAnnotation(AnnotationSpec.builder(ClassName("android.annotation", "SuppressLint")).addMember(""""WrongViewCast"""").build())
+        val funOnCreateView = FunSpec.builder("onCreateView")
+            .addModifiers(KModifier.OVERRIDE)
+            .addParameter("inflater", ClassName("android.view", "LayoutInflater"))
+            .addParameter("container", viewGroup.copy(nullable = true))
+            .addParameter("savedInstanceState", ClassName("android.os", "Bundle").copy(nullable = true))
+            .addCode("""return inflater.inflate($layoutId, container, false)""".trimIndent())
+            .returns(view.copy(nullable = true))
+            .build()
+
+        return clazz.addFunction(funOnCreateView)
     }
 }
 
--- src/main/kotlin/mikep/fragmentcreator/FragmentCreator.kt
+++ src/main/kotlin/mikep/fragmentcreator/FragmentCreator.kt
@@ -1,38 +1,60 @@
 package mikep.fragmentcreator
 
+import com.squareup.kotlinpoet.*
 import org.w3c.dom.Document
 import org.w3c.dom.NamedNodeMap
 import org.w3c.dom.NodeList
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
-class FragmentCreator(filePath: String, className: String, outputPath: String) {
+class FragmentCreator(filePath: String, val className: String, outputPath: String) {
 
     private val xmlDocument: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(File(filePath))
-    private var clazz: String = "class $className : Fragment() {\n"
     private val regex = Regex("\\w+\\.xml")
     private val layoutName = "R.layout.${regex.find(filePath)?.value?.replace(".xml", "")}"
-    private val file = File("$outputPath\\$className.kt")
+    private var clazz = AndroidClasses.createFragmentClass(className, layoutName)
+    private val file = File(outputPath)
     fun buildFragmentClass(){
-        printChildNodes(xmlDocument.childNodes.item(0).childNodes)
-        clazz = clazz.plus("\n\n\toverride fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {\n\t\treturn inflater.inflate($layoutName, container, false)\n\t}")
-        clazz = clazz.plus("\n\n}")
-        print(clazz)
-        file.createNewFile()
-        file.writeText(clazz)
 
+        createViewValues(xmlDocument.childNodes.item(0).childNodes)
+
+        print(clazz.toString())
+        file.createNewFile()
+        FileSpec.builder("", className).addType(clazz.build()).build().writeTo(file)
     }
 
-    private fun printChildNodes(nodeList: NodeList) {
+    private fun createViewValues(nodeList: NodeList) {
         for(i in 1 until nodeList.length step 2) {
             val node = nodeList.item(i)
             node.attributes.androidIdOrNull?.let {
-                val nodeName = if(node.nodeName == "include") "ViewGroup" else node.nodeName
-                clazz = clazz.plus("\n\tprivate val $it: $nodeName by findViewById(${it.rId})")
+                createValue(it, node.nodeName)
             }
-            if(node.hasChildNodes()) printChildNodes(node.childNodes)
+            if(node.hasChildNodes()) createViewValues(node.childNodes)
         }
     }
+
+    private fun createValue(valueName: String, nodeName: String) {
+        val valueNameParts = valueName.split("_")
+        var propertyName = ""
+        valueNameParts.forEachIndexed { index, s ->
+            if(index > 0) {
+                val string = s.replace(s.first(),s.first().toUpperCase())
+                propertyName = propertyName.plus(string)
+            } else {
+                propertyName = propertyName.plus(s)
+            }
+        }
+        clazz.addProperty(
+            PropertySpec.builder(
+                propertyName,
+                AndroidClasses.classNameFromNodeName(nodeName)
+            ).delegate(
+                CodeBlock.builder()
+                    .addStatement("findViewById(${valueName.rId})")
+                    .build())
+            .build())
+    }
+
 }
 
 val NamedNodeMap.androidIdOrNull: String?
